4a3eef23cb23a51ba32ff4edb248a4dccaac5464
John Francis <john9francis@gmail.com>
2024-02-04 19:17:41 -0700

    setup the basic voice recognition

---
+++ interface.py
@@ -0,0 +1,31 @@
+import speech_recognition
+
+def listen():
+  recognizer = speech_recognition.Recognizer()
+  with speech_recognition.Microphone() as source:
+    print("Listening...")
+    audio = recognizer.listen(source)
+    return audio
+  
+
+def audio_to_text(audio):
+  recognizer = speech_recognition.Recognizer()
+  try:
+    text = recognizer.recognize_google(audio)
+    return text
+  except:
+    return ""
+  
+
+
+def main():
+  while True:
+    audio = listen()
+    user_sentence = audio_to_text(audio)
+    print(user_sentence)
+
+
+
+
+if __name__ == "__main__":
+  main()
