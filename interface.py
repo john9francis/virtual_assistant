9f0725d5035912999347678be5ce50a2c88c21e8
John Francis <john9francis@gmail.com>
2024-02-04 19:27:19 -0700

    Added useful comments and a 'stop' condition that makes the assistent stop listening.

--- interface.py
+++ interface.py
@@ -1,6 +1,10 @@
 import speech_recognition
 
 def listen():
+  '''
+  Uses the microphone to get an audio input and returns it
+  as an AudioData object.
+  '''
   recognizer = speech_recognition.Recognizer()
   with speech_recognition.Microphone() as source:
     print("Listening...")
@@ -9,6 +13,10 @@ def listen():
   
 
 def audio_to_text(audio):
+  '''
+  Takes in an AudioData object, parses it, and returns
+  the audio as a string. 
+  '''
   recognizer = speech_recognition.Recognizer()
   try:
     text = recognizer.recognize_google(audio)
@@ -19,9 +27,19 @@ def audio_to_text(audio):
 
 
 def main():
-  while True:
+
+  running = True
+
+  while running:
+    # infinitely listen and parse inputs
     audio = listen()
     user_sentence = audio_to_text(audio)
+
+    # if the user says stop, end the loop
+    if user_sentence == "stop":
+      running = False
+
+    # for now, just print the sentence
     print(user_sentence)
 
 
